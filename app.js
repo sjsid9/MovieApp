22a27233a52aa202ee040110d48dbc89347201cb
sid <sjsid9@gmail.com>
2020-01-05 21:47:01 +0530

    Added search for movies
    Signed-off-by: sid <sjsid9@gmail.com>

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+var bodyParser = require("body-parser");
+
+var movie;
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/search", (req, res) => {
+    movie = req.body.movieName;
+    // console.log(movie);
+    res.redirect("/results");
+});
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
 
 app.get("/results", (req, res) => {
-    request("http://www.omdbapi.com/?s=california&apikey=thewdb", (error, response, body) => {
+    request("http://www.omdbapi.com/?s=" + movie + "&apikey=thewdb", (error, response, body) => {
         if (!error && response.statusCode == 200) {
-            var results = JSON.parse(body);
-            res.send(results.Search[0].Title);
+            var data = JSON.parse(body);
+            res.render("results", { data: data });
+            // res.send(data.Search[0].Title);
         }
     });
 });
